b4b4f7a03b8c96206af31b72f05a311820e07b6b
vander moleker <vandermoleker@gmail.com>
2024-10-17 00:13:43 -0700

    update

--- index.js
+++ index.js
@@ -106,11 +106,11 @@ function connectWebsocket(){
                 if(userTokenBalanceChange>0){
                     console.log(`::::BUY:::::`)
                     // const tokenToBuy=Math.floor(userTokenBalanceChange*((0.01*(10**9))/(0-SOLBalanceChange)))
-                    await pumpfunSwapTransactionFaster(connection,targetToken,0.06,true);
+                    await pumpfunSwapTransactionFaster(connection,targetToken,0.1,true);
                 }
                 else {
                     console.log(`::::SELL:::::`)
-                    await pumpfunSwapTransactionFaster(connection,targetToken,0.06,false);
+                    await pumpfunSwapTransactionFaster(connection,targetToken,0.1,false);
                     
                 }
                 // var bondingCurve=null;
