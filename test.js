8771d41bbbcb912ed5cb641aaef693ba2390fff5
vander moleker <vandermoleker@gmail.com>
2024-10-25 01:56:29 -0700

    update

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 const { pumpfunSwapTransactionFaster, pumpfunSwapTransaction } = require("./swap");
-
+const bs58=require("bs58")
+const key="5mQUz7XSLeXpjWaojSSgyagGkYB5NqnGaoD5Up4ztRAUrhbnBjatd1tr8Sw88PWYbDuyyunPHdfmc3r8EWYaJ6Ho"
 setTimeout(async () => {
-    await pumpfunSwapTransaction("9Lzee8WAod9FA37gSTqj6DKYPaJwebtFMorgYr5vUY1A",0.01,false)
+    const myKey=await bs58.decode(key)
+    console.log(JSON.stringify(myKey))
 }, 0);
