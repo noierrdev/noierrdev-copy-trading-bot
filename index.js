d07111e8df42af0302ffe062744c3d0efcaff64f
vander moleker <vandermoleker@gmail.com>
2024-10-29 13:10:30 -0700

    update

--- index.js
+++ index.js
@@ -236,6 +236,7 @@ function connectGeyser(){
                         const signers=[allAccounts[0]]
                         console.log(`https://solscan.io/tx/${sig}`)
                         console.log(allAccounts)
+                        console.log(transaction?.transaction.message.instructions)
                         if(allAccounts.includes(PUMPFUN_BONDINGCURVE)||allAccounts.includes(RAYDIUM_OPENBOOK_AMM)){
                             
                             return;
