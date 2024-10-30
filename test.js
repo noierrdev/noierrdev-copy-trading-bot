151d275625415755e617475464c245cac8ebd910
vander moleker <vandermoleker@gmail.com>
2024-10-30 07:12:47 -0700

    update

--- test.js
+++
@@ -1,7 +0,0 @@
-const { pumpfunSwapTransactionFaster, pumpfunSwapTransaction } = require("./swap");
-const bs58=require("bs58")
-const key="5mQUz7XSLeXpjWaojSSgyagGkYB5NqnGaoD5Up4ztRAUrhbnBjatd1tr8Sw88PWYbDuyyunPHdfmc3r8EWYaJ6Ho"
-setTimeout(async () => {
-    const myKey=await bs58.decode(key)
-    console.log(JSON.stringify(myKey))
-}, 0);
