9314207fd9b4d96ed2268f9ea123ee6544d4aa41
vander moleker <vandermoleker@gmail.com>
2024-10-30 03:53:59 -0700

    update

--- index.js
+++ index.js
@@ -140,25 +140,25 @@ function connectWebsocket(){
                     if(userTokenBalanceChange>0){
                         console.log(`::::BUY:::::`)
                         const tokenToBuy=Math.floor(userTokenBalanceChange*((0.1*(10**9))/(0-SOLBalanceChange)))
-                        await swapPumpfunFaster(connection,targetToken,bondingCurve,bondingCurveVault,tokenToBuy,true);
-                        // await pumpfunSwapTransactionFaster(connection,targetToken,0.1,true);
+                        // await swapPumpfunFaster(connection,targetToken,bondingCurve,bondingCurveVault,tokenToBuy,true);
+                        await pumpfunSwapTransactionFasterWallet(connection,wallet,targetToken,0.1,true);
                     }
                     else {
                         console.log(`::::SELL:::::`)
                         // await swapPumpfunFaster(connection,targetToken,bondingCurve,bondingCurveVault,10000,false);
-                        await pumpfunSwapTransactionFaster(connection,targetToken,0.15,false);
+                        await pumpfunSwapTransactionFasterWallet(connection, wallet,targetToken,0.15,false);
                         
                     }
                 }else{
                     if(userTokenBalanceChange>0){
                         console.log(`::::BUY:::::`)
                         // const tokenToBuy=Math.floor(userTokenBalanceChange*((0.1*(10**9))/(0-SOLBalanceChange)))
-                        await pumpfunSwapTransactionFaster(connection,targetToken,0.1,true);
+                        await pumpfunSwapTransactionFasterWallet(connection, wallet,targetToken,0.1,true);
                         // await bot.api.sendMessage(`noierrdevcopytrading_channel`,`<b>Pumpfun copied!</b>\n<code>${signers[0]}</code>\n<a href="https://solscan.io/tx/${signature}" >Photon</a>`,{parse_mode:"HTML",link_preview_options:{is_disabled:true}})
                     }
                     else {
                         console.log(`::::SELL:::::`)
-                        await pumpfunSwapTransactionFaster(connection,targetToken,0.15,false);
+                        await pumpfunSwapTransactionFasterWallet(connection, wallet,targetToken,0.15,false);
                         
                     }
                 }
