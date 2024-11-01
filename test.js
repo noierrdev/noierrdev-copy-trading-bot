98f139c6a8e6acb51d4f6284e12dc7f82da4b1dd
vander moleker <vandermoleker@gmail.com>
2024-11-01 13:10:44 -0700

    update

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 const { Keypair, Connection } = require("@solana/web3.js");
-const { pumpfunSwapTransactionFasterWallet, pumpfunSwapTransactionFasterWalletToken, swapPumpfunFasterWallet } = require("./swap");
+const { pumpfunSwapTransactionFasterWallet, pumpfunSwapTransactionFasterWalletToken, swapPumpfunFasterWallet, swapPumpfunFasterWalletStaked } = require("./swap");
 
 require("dotenv").config();
 const connection=new Connection(process.env.RPC_API)
+const stakedConnection=new Connection(process.env.STAKED_RPC)
 const PRIVATE_KEY =new  Uint8Array(JSON.parse(process.env.PRIVATE_KEY));
 const wallet = Keypair.fromSecretKey(PRIVATE_KEY);
 
@@ -11,6 +12,7 @@ const bondingCurve="3mm3P9fC899FzM5BQrt5SQRyMZD5aeFS9ywxUDMLW4cK"
 const bondingCurveVault="5masFP6hcRRaqYp92pGkGi8zQj1WSxXZj9iVg8vP1kcT"
 
 setTimeout(async () => {
-    await pumpfunSwapTransactionFasterWalletToken(connection,wallet,"4RXLAJQHhznjNaa4Auep3u1rA5XeKz4Ht2KWVf8ku2NK",100000,true)
+    // await pumpfunSwapTransactionFasterWalletToken(connection,wallet,"4RXLAJQHhznjNaa4Auep3u1rA5XeKz4Ht2KWVf8ku2NK",100000,true)
+    await swapPumpfunFasterWalletStaked(connection,stakedConnection,wallet,targetToken,bondingCurve,bondingCurveVault,100,true)
     // await swapPumpfunFasterWallet(connection,wallet,targetToken,bondingCurve,bondingCurveVault,100,false)
 }, 0);
