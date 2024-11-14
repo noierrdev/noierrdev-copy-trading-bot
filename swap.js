6ffa65605fc5fcfa38565dfe3e1fd2507b84be9e
vander moleker <vandermoleker@gmail.com>
2024-11-14 11:31:30 -0800

    update

--- swap.js
+++ swap.js
@@ -6707,7 +6707,7 @@ const pumpfunSwapTransactionFasterWalletStaked=async (connection,stakedConnectio
           "denominatedInSol": buy?'true':'false',
           "amount": buy?String(amount):"100%",
           "slippage": 10, 
-          "priorityFee": 0.0006, 
+          "priorityFee": buy?0.001:0.0006, 
           "pool": "pump"
       })
   });
