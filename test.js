d7bbc14e0b5ba61bf5d56ff74bd9c53c15850d0e
vander moleker <vandermoleker@gmail.com>
2024-11-14 12:43:25 -0800

    update

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
-const { Keypair, Connection } = require("@solana/web3.js");
+const { Keypair, Connection, PublicKey } = require("@solana/web3.js");
 const { pumpfunSwapTransactionFasterWallet, pumpfunSwapTransactionFasterWalletToken, swapPumpfunFasterWallet, swapPumpfunFasterWalletStaked, pumpfunSwapTransactionFasterWalletStaked, pumpfunSwapTransactionFasterWalletTokenStaked } = require("./swap");
+const { getAssociatedTokenAddressSync } = require("@solana/spl-token");
 
 require("dotenv").config();
 const connection=new Connection(process.env.RPC_API)
@@ -13,6 +14,17 @@ const bondingCurveVault="2xTpNUhJdsm29CaCQWufb3kE1AiyvWiHB1J6Sc7AqL9V"
 
 setTimeout(async () => {
     // await pumpfunSwapTransactionFasterWalletStaked(connection,stakedConnection,wallet,targetToken,0.001,false)
-    await swapPumpfunFasterWalletStaked(connection,stakedConnection,wallet,targetToken,bondingCurve,bondingCurveVault,10000,true)
+    await swapPumpfunFasterWalletStaked(connection,stakedConnection,wallet,targetToken,bondingCurve,bondingCurveVault,10000,false)
     // await swapPumpfunFasterWallet(connection,wallet,targetToken,bondingCurve,bondingCurveVault,100,false)
+    const tokenAccount=getAssociatedTokenAddressSync(new PublicKey(targetToken),wallet.publicKey);
+    var intervalId=setInterval(async () => {
+        try {
+            const tokenBalance=await connection.getTokenAccountBalance(tokenAccount);
+            console.log(tokenBalance.value.amount)
+            clearInterval(intervalId)
+        } catch (error) {
+            console.log(error)
+        }
+        
+    }, 500);
 }, 0);
