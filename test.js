b3798561adb00be8b6570180f5c9d0d54362f515
vander moleker <vandermoleker@gmail.com>
2024-11-17 00:10:31 -0800

    update

--- test.js
+++ test.js
@@ -13,18 +13,18 @@ const bondingCurve="5P3gR9zAujZyDwyxYMa3QUfXpQAW5ZeXKJmM7aKRC2MK"
 const bondingCurveVault="2xTpNUhJdsm29CaCQWufb3kE1AiyvWiHB1J6Sc7AqL9V"
 
 setTimeout(async () => {
-    // await pumpfunSwapTransactionFasterWalletStaked(connection,stakedConnection,wallet,targetToken,0.001,false)
-    await swapPumpfunFasterWalletStaked(connection,stakedConnection,wallet,targetToken,bondingCurve,bondingCurveVault,10000,false)
-    // await swapPumpfunFasterWallet(connection,wallet,targetToken,bondingCurve,bondingCurveVault,100,false)
-    const tokenAccount=getAssociatedTokenAddressSync(new PublicKey(targetToken),wallet.publicKey);
-    var intervalId=setInterval(async () => {
-        try {
-            const tokenBalance=await connection.getTokenAccountBalance(tokenAccount);
-            console.log(tokenBalance.value.amount)
-            clearInterval(intervalId)
-        } catch (error) {
-            console.log(error)
-        }
+    // await pumpfunSwapTransactionFasterWalletStaked(connection,stakedConnection,wallet,targetToken,0.001,true)
+    await swapPumpfunFasterWallet(connection,wallet,targetToken,bondingCurve,bondingCurveVault,100000,false)
+    // await swapPumpfunFasterWallet(connection,wallet,targetToken,bondingCurve,bondingCurveVault,10000,true)
+    // const tokenAccount=getAssociatedTokenAddressSync(new PublicKey(targetToken),wallet.publicKey);
+    // var intervalId=setInterval(async () => {
+    //     try {
+    //         const tokenBalance=await connection.getTokenAccountBalance(tokenAccount);
+    //         console.log(tokenBalance.value.amount)
+    //         clearInterval(intervalId)
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
         
-    }, 500);
+    // }, 500);
 }, 0);
