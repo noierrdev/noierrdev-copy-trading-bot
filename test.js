1b10651a12b288ee02bc48054a8e4b889eca7596
vander moleker <vandermoleker@gmail.com>
2024-11-22 11:11:13 -0800

    update

--- test.js
+++ test.js
@@ -8,13 +8,13 @@ const stakedConnection=new Connection(process.env.STAKED_RPC)
 const PRIVATE_KEY =new  Uint8Array(JSON.parse(process.env.PRIVATE_KEY));
 const wallet = Keypair.fromSecretKey(PRIVATE_KEY);
 
-const targetToken="2WwUfbtxoQdaf9W7CSs5BmSXaFBRUvMsufHC566upump"
+const targetToken="Fisaf7JKjowDAYpeHmtmnScBgbUQyZt7udc1Hp3qpump"
 const bondingCurve="5P3gR9zAujZyDwyxYMa3QUfXpQAW5ZeXKJmM7aKRC2MK"
 const bondingCurveVault="2xTpNUhJdsm29CaCQWufb3kE1AiyvWiHB1J6Sc7AqL9V"
 
 setTimeout(async () => {
-    // await pumpfunSwapTransactionFasterWalletStaked(connection,stakedConnection,wallet,targetToken,0.001,true)
-    await swapPumpfunFasterWallet(connection,wallet,targetToken,bondingCurve,bondingCurveVault,100000,false)
+    await pumpfunSwapTransactionFasterWalletStaked(connection,stakedConnection,wallet,targetToken,0.001,false)
+    // await swapPumpfunFasterWallet(connection,wallet,targetToken,bondingCurve,bondingCurveVault,100000,false)
     // await swapPumpfunFasterWallet(connection,wallet,targetToken,bondingCurve,bondingCurveVault,10000,true)
     // const tokenAccount=getAssociatedTokenAddressSync(new PublicKey(targetToken),wallet.publicKey);
     // var intervalId=setInterval(async () => {
