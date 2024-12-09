2708031b2bb85b46135e7111fc2239661d23de6b
vander moleker <vandermoleker@gmail.com>
2024-12-09 00:40:51 -0800

    update

--- index.js
+++ index.js
@@ -173,6 +173,7 @@ function connectGeyser(){
                                         if(!swapInstruction) return;
                                         const targetToken=allAccounts[swapInstruction.accounts[2]];
                                         const userPostTokenBalance=transaction.meta.postTokenBalances.find(ba=>((ba.mint==targetToken)&&(ba.owner==wallet.publicKey.toBase58())));
+                                        if(!allTrades[targetToken]) allTrades[targetToken]={}
                                         allTrades[targetToken].amount=userPostTokenBalance.uiTokenAmount.uiAmount;
                                     }else if(userTokenBalanceChange<0){
                                         // MY RAYDIUM SELL
@@ -245,6 +246,7 @@ function connectGeyser(){
                                         console.log(`https://solscan.io/tx/${sig}`)
                                         console.log(`::::BUY:::::`)
                                         const tokenToBuy=Math.floor(userTokenBalanceChange*((0.001*(10**9))/(0-SOLBalanceChange)))
+                                        allTrades[targetToken]={}
                                         await swapPumpfunWalletFastest(connection,stakedConnection,wallet,targetToken,bondingCurve,bondingCurveVault,tokenToBuy,true);
                                     }
                                     else {
